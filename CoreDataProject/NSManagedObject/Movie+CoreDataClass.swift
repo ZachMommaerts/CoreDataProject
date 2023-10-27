ba39bb1589ff9894f987eae141545b647c03010d
ZachMommaerts <zmommaerts@gmail.com>
2023-10-27 14:03:54 -0500

    - Add NSManagedObject lesson

---
+++ CoreDataProject/NSManagedObject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Zach Mommaerts on 10/27/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ CoreDataProject/NSManagedObject/NSManagedObjectSubclassesView.swift
@@ -0,0 +1,18 @@
+//
+//  NSManagagedObjectSubclassesView.swift
+//  CoreDataProject
+//
+//  Created by Zach Mommaerts on 10/27/23.
+//
+
+import SwiftUI
+
+struct NSManagedObjectSubclassesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NSManagedObjectSubclassesView()
+}
