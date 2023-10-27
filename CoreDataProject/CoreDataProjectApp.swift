7f39b49b42c9d26887f4e12ee6a63ada4570d6c6
ZachMommaerts <zmommaerts@gmail.com>
2023-10-27 12:52:29 -0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Zach Mommaerts on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
